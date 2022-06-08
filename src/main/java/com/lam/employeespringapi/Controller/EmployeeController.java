3207bb7b7d572c7373aba4adaa50ad19dfc5ebfe
LuongViLam <kulam1782000@gmail.com>
2022-06-08 18:31:01 +0700

    comment cross origin

--- src/main/java/com/lam/employeespringapi/Controller/EmployeeController.java
+++ src/main/java/com/lam/employeespringapi/Controller/EmployeeController.java
@@ -13,7 +13,7 @@ import java.util.List;
 import java.util.Map;
 
 
-@CrossOrigin(origins = "http://localhost:3000")
+//@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
